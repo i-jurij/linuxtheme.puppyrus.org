576dbf3442dc94c5196a9b43f554ba7faf2db78e
i-jurij <38424205+i-jurij@users.noreply.github.com>
2023-12-19 15:57:07 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,26 +7,6 @@ header('Content-Type: text/html; charset=utf-8');
 include './php/gallery.php';
 //----------------------------------------------------------------
 
-/*
-//Kontroller ------------------------------------------------
-if(!isset($_GET['one'])&&!isset($_GET['two'])&&!isset($_GET['thre'])) {
-	$one = 'home';
-   include 'controllers/home.php';
-}
-else {
-	$one = addslashes(strip_tags(trim($_GET['one'])));
-	if(file_exists('controllers/'.$one.'.php')) {
-   	include 'controllers/'.$one.'.php' ;
-   }
-   else {
-		header("HTTP/1.1 404 Not Found");
-		$one = 'home';
-		include 'controllers/home.php';
-		$read='ОШИБКА 404! Данной страницы не существует'; 
-		$content = '';  
-   }
-}
-*/
 // vyvod stranizy iz shablona -------------------------------
 include './tpl/index.tpl' ;
 
